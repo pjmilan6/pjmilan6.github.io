28427c9ca6ef157a0d9c5808c8cbd941cb66ebd7
pjmilan6 <155210810+pjmilan6@users.noreply.github.com>
2025-05-09 20:16:12 +0000

    Deploying to gh-pages from @ pjmilan6/pjmilan6.github.io@1b48bf86851e72594578606726c8d118ee44445c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-tutorial-proposal-on-programming-novel-ai-accelerators-for-scientific-computing-in-collab-with-argonne-national-laboratory-was-accepted-and-we-will-be-presenting-it-at-isc-high-performance-in-hamburg-germany-on-june-13-2025",
+          title: 'Our tutorial proposal on Programming Novel AI Accelerators for Scientific Computing, in collab...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
